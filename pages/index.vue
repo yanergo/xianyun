727cb71da460dafef93e7dcf3b955c6b430ffaa8
yanergo <53567789+yanergo@users.noreply.github.com>
2019-10-12 11:50:27 +0800

    首页tab栏切换

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,41 @@
         <!-- 轮播图 -->
         <el-carousel :interval="5000" arrow="always">
             <el-carousel-item v-for="(item, index) in banners" :key="index">
-                <div class="banner_image" :style="`
-                background:url(${$axios.defaults.baseURL + item.url}) center center no-repeat;
-                bcakground-size:contain contain;`"></div>
+                <div
+                    class="banner_image"
+                    :style="
+                        `
+                background:url(${$axios.defaults.baseURL +
+                    item.url}) center center no-repeat;
+                bcakground-size:contain contain;`
+                    "
+                ></div>
             </el-carousel-item>
         </el-carousel>
+        <!-- 搜索框 -->
+        <div class="banner_content">
+            <div class="search_bar">
+                <!-- tab栏 -->
+                <el-row type="flex" class="search_tab">
+                    <span
+                        v-for="(item, index) in options"
+                        :key="index"
+                        @click="handleTabChange(index)"
+                        :class="{ active: current == index }"
+                    >
+                        <i>{{ item.title }}</i>
+                    </span>
+                </el-row>
+                <!-- 输入框 -->
+                <el-row type="flex" align="middle" class="search_input">
+                    <input
+                        type="text"
+                        :placeholder="options[current].placeholder"
+                    />
+                    <i class="el-icon-search"></i>
+                </el-row>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,8 +45,30 @@
 export default {
     data() {
         return {
-            banners: []
+            banners: [],
+            options: [
+                { title: "攻略", placeholder: "搜索城市" },
+                { title: "酒店", placeholder: "请输入城市搜索酒店" },
+                { title: "机票", placeholder: "" }
+            ],
+            // options2: {
+            // 	"攻略": "搜索城市",
+            // 	"酒店": "请输入城市搜索酒店",
+            // 	"机票": ""
+            // },
+
+            // tab栏的索引
+            current: 0
         };
+    },
+    methods:{
+        handleTabChange(index){
+            this.current = index;
+
+            if(index === 2){
+                this.$router.push('/air');
+            }
+        }
     },
     mounted() {
         this.$axios({
@@ -31,18 +83,103 @@ export default {
 </script>
 
 <style scoped lang="less">
-.container{
+.container {
     min-width: 1000px;
     margin: 0 auto;
     position: relative;
 
-    /deep/ .el-carousel__container{
+    /deep/ .el-carousel__container {
         height: 700px;
     }
 
-    .banner_image{
+    .banner_image {
         width: 100%;
         height: 100%;
+    }
+
+    .banner_content {
+        z-index: 9;
+        width: 1000px;
+        position: absolute;
+        left: 50%;
+        top: 45%;
+        margin-left: -500px;
+        border-top: 1px transparent solid;
+
+        .search_bar {
+            width: 552px;
+            margin: 0 auto;
+        }
+
+        .search_tab {
+            .active {
+                i {
+                    color: #333;
+                }
+                &::after {
+                    background-color: #eee;
+                }
+            }
+            span {
+                width: 82px;
+                height: 36px;
+                display: block;
+                position: relative;
+                margin-right: 8px;
+                cursor: pointer;
+                i {
+                    position: absolute;
+                    z-index: 2;
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                    line-height: 30px;
+                    text-align: center;
+                    color: #fff;
+                }
+                &:after {
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    display: block;
+                    content: "";
+                    width: 100%;
+                    height: 100%;
+                    border: 1px rgba(255, 255, 255, 0.2) solid;
+                    border-bottom: none;
+                    transform: scale(1.1, 1.3) perspective(0.7em)
+                        rotateX(2.2deg);
+                    transform-origin: bottom left;
+                    background: rgba(0, 0, 0, 0.5);
+                    border-radius: 1px 2px 0 0;
+                    box-sizing: border-box;
+                }
+            }
+        }
+
+        .search_input {
+            width: 550px;
+            height: 46px;
+            background: #fff;
+            border-radius: 0 4px 4px 4px;
+            border: 1px rgba(255, 255, 255, 0.2) solid;
+            border-top: none;
+            box-sizing: unset;
+            input {
+                flex: 1;
+                height: 20px;
+                padding: 13px 15px;
+                outline: none;
+                border: 0;
+                font-size: 16px;
+            }
+            .el-icon-search {
+                cursor: pointer;
+                font-size: 22px;
+                padding: 0 10px;
+                font-weight: bold;
+            }
+        }
     }
 }
 </style>
