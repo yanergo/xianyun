d45aa63c86299eb325d2dab06af6879642201ba8
yanergo <53567789+yanergo@users.noreply.github.com>
2019-10-15 14:06:52 +0800

    机票列表布局完成

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -40,15 +40,50 @@
         </h2>
 
         <!-- 特价机票 -->
-        <div class="air-sale"></div>
+        <div class="air-sale">
+            <el-row type="flex" class="air-sale-pic" justify="space-between">
+                <el-col :span="6" v-for="(item, index) in sales" :key="index">
+                    <nuxt-link
+                        :to="
+                            `/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`
+                        "
+                    >
+                        <img :src="item.cover" />
+                        <el-row
+                            class="layer-bar"
+                            type="flex"
+                            justify="space-between"
+                        >
+                            <span
+                                >{{ item.departCity }}-{{ item.destCity }}</span
+                            >
+                            <span>￥{{item.price}}</span>
+                        </el-row>
+                    </nuxt-link>
+                </el-col>
+            </el-row>
+        </div>
     </section>
 </template>
 
 <script>
 import SearchForm from "@/components/air/searchForm";
 export default {
-    components:{
+    components: {
         SearchForm
+    },
+    data(){
+        return {
+            sales:[]
+        };
+    },
+    mounted(){
+        this.$axios({
+            url:'/airs/sale'
+        }).then(res=>{
+            const {data} = res.data;
+            this.sales = data;
+        })
     }
 };
 </script>
