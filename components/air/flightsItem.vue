a58ee38b80dc4c8e27d92eac7da1e2f352c0b11d
yanergo <53567789+yanergo@users.noreply.github.com>
2019-10-15 15:59:07 +0800

    计算航班出发到达间隔时间

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -3,7 +3,9 @@
         <div>
             <!-- 显示的机票信息 -->
             <el-row type="flex" align="middle" class="flight-info">
-                <el-col :span="6"> <span>{{item.airline_name}} </span> {{item.flight_no}} </el-col>
+                <el-col :span="6">
+                    <span>{{ item.airline_name }} </span> {{ item.flight_no }}
+                </el-col>
                 <el-col :span="12">
                     <el-row
                         type="flex"
@@ -11,20 +13,29 @@
                         class="flight-info-center"
                     >
                         <el-col :span="8" class="flight-airport">
-                            <strong>{{item.dep_time}}</strong>
-                            <span>{{item.org_airport_name}}{{item.org_airport_quay}}</span>
+                            <strong>{{ item.dep_time }}</strong>
+                            <span
+                                >{{ item.org_airport_name
+                                }}{{ item.org_airport_quay }}</span
+                            >
                         </el-col>
                         <el-col :span="8" class="flight-time">
-                            <span>2时20分</span>
+                            <span>{{rankTime}}</span>
                         </el-col>
                         <el-col :span="8" class="flight-airport">
-                            <strong>{{item.arr_time}}</strong>
-                            <span>{{item.dst_airport_name}}{{item.dst_airport_quay}}</span>
+                            <strong>{{ item.arr_time }}</strong>
+                            <span
+                                >{{ item.dst_airport_name
+                                }}{{ item.dst_airport_quay }}</span
+                            >
                         </el-col>
                     </el-row>
                 </el-col>
                 <el-col :span="6" class="flight-info-right">
-                    ￥<span class="sell-price">{{item.seat_infos[0].org_settle_price_child}}</span>起
+                    ￥<span class="sell-price">{{
+                        item.seat_infos[0].org_settle_price_child
+                    }}</span
+                    >起
                 </el-col>
             </el-row>
         </div>
@@ -40,16 +51,17 @@
                         class="flight-sell"
                     >
                         <el-col :span="16" class="flight-sell-left">
-                            <span>{{item.seat_infos[0].name}}</span> | {{item.seat_infos[0].supplierName}}
+                            <span>{{ item.seat_infos[0].name }}</span> |
+                            {{ item.seat_infos[0].supplierName }}
                         </el-col>
                         <el-col :span="5" class="price">
-                            ￥{{item.seat_infos[0].org_settle_price}}
+                            ￥{{ item.seat_infos[0].org_settle_price }}
                         </el-col>
                         <el-col :span="3" class="choose-button">
                             <el-button type="warning" size="mini">
                                 选定
                             </el-button>
-                            <p>剩余：{{item.seat_infos[0].discount}}</p>
+                            <p>剩余：{{ item.seat_infos[0].discount }}</p>
                         </el-col>
                     </el-row>
                 </el-col>
@@ -61,13 +73,40 @@
 <script>
 export default {
     //props可以使用数组也可以使用对象
-    props:{
+    props: {
         // item是声明组件可以接受item属性
-        item:{
+        item: {
             // 声明item的类型
-            type:Object,
+            type: Object,
             // 默认为空对象，不传值时
-            default:{}
+            default: {}
+        }
+    },
+    computed: {
+        rankTime() {
+            const depTime = this.item.dep_time.split(":");
+            const arrTime = this.item.arr_time.split(":");
+
+            // 到达时间为第二天
+            if (arrTime[0] < depTime[0]) {
+                arrTime[0] += 24;
+            }
+
+            // 出发分钟时间
+            const start = depTime[0] * 60 + depTime[1] * 1;
+
+            // 到达分钟时间
+            const end = arrTime[0] * 60 + arrTime[1] * 1;
+
+            // 相隔分钟时间
+            const dis = end - start;
+
+            // 换算成小时和分钟
+            const hour = Math.floor(dis / 60);
+
+            const min = dis % 60;
+
+            return `${hour}小时${min}分钟`
         }
     }
 };
